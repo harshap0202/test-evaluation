6f25ed487d81638a18ee35bb92b84aac54ac338e
harshap0202 <02harshpatil@gmail.com>
2024-07-17 00:08:21 +0530

    task 1 spacing update

--- task1.sh
+++ task1.sh
@@ -12,16 +12,12 @@ HOUR=$(echo "$INP_HOUR" | tr -d :)
 
 if [ $HOUR -ge 0000 ] && [ $HOUR -le 0600 ]; then 
     echo "Early Morning"
-
 elif [ $HOUR -ge 0601 ] && [ $HOUR -le 1300 ]; then
     echo "Morning"
-
 elif [ $HOUR -ge 1301 ] && [ $HOUR -le 1800 ]; then
     echo "Afternoon"
-
 elif [ $HOUR -ge 1801 ] && [ $HOUR -le 2359 ]; then
     echo "Late Night"
-
 else
     echo "Invalid hour. Enter valid hour in 24-hour format (e.g. 14:56)"
     exit 1
